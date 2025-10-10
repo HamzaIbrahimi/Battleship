b131516a39518692ff8a5576f921d2e5e9a90f5f
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-10-10 23:43:46 +0200

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import './css/normalize.css';
 import './css/style.css';
-import displayGrids from './util';
-
-const div = document.querySelector('.player1');
-const div1 = document.querySelector('.player2');
-displayGrids(div);
-displayGrids(div1);
-
-div.addEventListener('click', (e) => {
-    if (e.target) {
-        console.log(e.currentTarget.c);
-    }
-});
