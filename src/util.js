1cd5b9b4bb4723d5e9820f09852dfbd99485683f
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-10-12 03:34:55 +0200

    Add a function to place a ship in the ship box

--- src/util.js
+++ src/util.js
@@ -1,9 +1,21 @@
-export default function displayGrids(container) {
+export function displayGrids(container) {
     for (let i = 0; i < 10; i += 1) {
         for (let j = 0; j < 10; j += 1) {
             const div = document.createElement('div');
             div.classList.add(`${i},${j}`);
             container.appendChild(div);
         }
+    }
+}
+
+export function createShip(container, shipName, length) {
+    const ship = document.createElement('div');
+    ship.className = shipName;
+    container.append(ship);
+    for (let i = 0; i < length; i += 1) {
+        ship.insertAdjacentHTML(
+            'beforeend',
+            `<div class="${shipName + i}"></div>`,
+        );
     }
 }
