019bcb2b72826f2105a498fc7c48b184fdce9fd4
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-10-12 05:40:06 +0200

    Add a function to render radio buttons

--- src/util.js
+++ src/util.js
@@ -2,7 +2,8 @@ export function displayGrids(container) {
     for (let i = 0; i < 10; i += 1) {
         for (let j = 0; j < 10; j += 1) {
             const div = document.createElement('div');
-            div.classList.add(`${i},${j}`);
+            div.classList.add('cell');
+            div.classList.add(`_${i}-${j}`);
             container.appendChild(div);
         }
     }
@@ -18,4 +19,22 @@ export function createShip(container, shipName, length) {
             `<div class="${shipName + i}"></div>`,
         );
     }
+}
+
+export function positionShipForm(container) {
+    container.insertAdjacentHTML(
+        'afterend',
+        `<div class="positioning">
+             <div>
+                 <input type="radio" name="positioning" id="vertical" checked>
+                 <label for="vertical">vertical</label>
+             </div>
+             <div>
+                 <input type="radio" name="positioning" id="horizontal">
+                 <label for="horizontal">horizontal</label>
+             </div>
+             <button id="start">start</button>
+             <button id="reset">reset</button>
+        </div>`,
+    );
 }
