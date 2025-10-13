624b01ff24b3ea8714a9901b0aa731fd0687492e
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-10-13 16:42:06 +0200

    Add ships to the player class

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,13 +1,19 @@
 import Gameboard from './Gameboard.js';
+import Ship from './Ship.js';
 
 export default class Player {
     #name;
 
-    gameBoard;
-
     constructor(name) {
         this.#name = name;
         this.gameBoard = new Gameboard();
+        this.shipsArray = [
+            new Ship('carrier', 5),
+            new Ship('battleship', 4),
+            new Ship('destroyer', 3),
+            new Ship('submarine', 3),
+            new Ship('patrolboat', 2),
+        ];
     }
 
     getName() {
