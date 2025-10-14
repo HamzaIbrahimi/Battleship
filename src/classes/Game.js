2d527931fef1f44c10906f5a3b1b2883a3678c57
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-10-14 23:57:03 +0200

    Enable the attack of the computer grid

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -1,6 +1,7 @@
 import container from './Containers.js';
 import DOMhelper from './DOMhelpers.js';
 import Player from './Player.js';
+import Ship from './Ship.js';
 
 export default class Game {
     constructor() {
@@ -21,64 +22,64 @@ export default class Game {
         container.playerGrid.addEventListener('click', (e) =>
             this.#placeShips(e),
         );
+        // container.playerGrid.addEventListener('click', (e) =>
+
+        // );
+        container.computerGrid.addEventListener('click', (e) =>
+            this.#turnPlayer(e),
+        );
         container.formArea.addEventListener('click', (e) => this.#reset(e));
         container.formArea.addEventListener('click', (e) => this.#start(e));
     }
 
     #placeShips(e) {
-        if (this.player.shipsArray.length !== 0) {
+        if (this.player.shipsArray.length !== 0 && e.target.matches('.cell')) {
             const currShip = this.player.shipsArray[0];
             this.player.gameBoard.removeShip(currShip);
-            if (e.target.matches('.cell')) {
-                const gridLocation = DOMhelper.findGridCellInClassName(
-                    e.target.className,
+
+            const gridLocation = DOMhelper.findGridCellInClassName(
+                e.target.className,
+            );
+
+            const position = document.querySelector(
+                'input[type="radio"]:checked',
+            ).id;
+            try {
+                this.player.gameBoard.placeShip(
+                    gridLocation,
+                    currShip,
+                    position,
                 );
-
-                const position = document.querySelector(
-                    'input[type="radio"]:checked',
-                ).id;
-                try {
-                    this.player.gameBoard.placeShip(
-                        gridLocation,
-                        currShip,
-                        position,
-                    );
-                    this.player.shipsArray.shift();
-                    if (this.player.shipsArray.length === 0) {
-                        container.instruction.textContent =
-                            'Your ships are ready, press start to play!';
-                    }
-                    const findLocations =
-                        this.player.gameBoard.findShip(currShip);
-
-                    DOMhelper.colorCells(
-                        'red',
-                        container.playerGrid.children,
-                        findLocations,
-                        0.5,
-                    );
-                    DOMhelper.createShip(
-                        container.playerOneShipArea,
-                        currShip.type(),
-                        currShip.length(),
-                    );
-                    DOMhelper.disablePointerEvents(
-                        container.playerGrid.children,
-                        findLocations,
-                    );
-                } catch (exc) {
-                    container.instruction.textContent = `Can't place ${currShip.type()} here!`;
+                this.player.shipsArray.shift();
+                if (this.player.shipsArray.length === 0) {
+                    container.instruction.textContent =
+                        'Your ships are ready, press start to play!';
                 }
+                const findLocations = this.player.gameBoard.findShip(currShip);
+
+                DOMhelper.colorCells(
+                    'red',
+                    container.playerGrid.children,
+                    findLocations,
+                    0.5,
+                );
+                DOMhelper.createShip(
+                    container.playerOneShipArea,
+                    currShip.type(),
+                    currShip.length(),
+                );
+                DOMhelper.disablePointerEvents(
+                    container.playerGrid.children,
+                    findLocations,
+                );
+            } catch (exc) {
+                container.instruction.textContent = `Can't place ${currShip.type()} here!`;
             }
         }
     }
 
     #viewShips(e) {
-        if (
-            e.target.matches('.cell') &&
-            e.target.style.backgroundColor !== 'red' &&
-            this.player.shipsArray.length !== 0
-        ) {
+        if (e.target.matches('.cell') && this.player.shipsArray.length !== 0) {
             container.instruction.textContent = `Place the ${this.player.shipsArray[0].type()}`;
             const gridLocation = DOMhelper.findGridCellInClassName(
                 e.target.className,
@@ -121,9 +122,9 @@ export default class Game {
     }
 
     #reset(e) {
-        container.instruction.textContent =
-            'Hover over grid to place ship, click when ready to place';
         if (e.target.matches('#reset')) {
+            container.instruction.textContent =
+                'Hover over grid to place ship, click when ready to place';
             DOMhelper.resetFunction(container.playerGrid.children);
             this.player = new Player('player');
             container.playerOneShipArea.innerHTML = '';
@@ -132,14 +133,11 @@ export default class Game {
 
     #start(e) {
         if (this.player.shipsArray.length === 0 && e.target.matches('#start')) {
+            console.log(this.computer.gameBoard.grid());
             container.instruction.textContent = 'Game has started!';
             const form = document.querySelector('.positioning');
             form.remove();
             DOMhelper.displayGrids(container.computerGrid);
-            DOMhelper.controlPointerEvents(
-                container.playerGrid.children,
-                'auto',
-            );
             while (this.computer.shipsArray.length !== 0) {
                 const currShip = this.computer.shipsArray.shift();
                 DOMhelper.createShip(
@@ -147,6 +145,52 @@ export default class Game {
                     currShip.type(),
                     currShip.length(),
                 );
+                DOMhelper.placeShipsRandomly(currShip, this.computer.gameBoard);
+                DOMhelper.controlPointerEvents(
+                    container.playerGrid.children,
+                    'none',
+                );
+                container.playerGrid.style.opacity = 0.5;
+                setTimeout(() => {
+                    container.instruction.textContent = `Its your turn ${this.player.getName()}, click on the opposite grid to attack their ship!`;
+                }, 1500);
+            }
+        }
+    }
+
+    #turnPlayer(e) {
+        if (this.player.shipsArray.length === 0 && e.target.matches('.cell')) {
+            const gridLocation = DOMhelper.findGridCellInClassName(
+                e.target.className,
+            );
+            const elem =
+                this.computer.gameBoard.getElementInLocation(gridLocation);
+            if (elem instanceof Ship) {
+                e.target.textContent = 'X';
+                e.target.style.backgroundColor = 'red';
+                container.instruction.textContent = `You attacked the opponent's ${elem.type()}!`;
+                const hit = elem.hits;
+                const shipArea = document.querySelector(
+                    `.player_two_ship_area .${elem.type()} .${elem.type() + hit}`,
+                );
+                shipArea.style.backgroundColor = 'red';
+                this.computer.gameBoard.receiveAttack(gridLocation);
+                e.target.style.pointerEvents = 'none';
+            } else if (elem === 0) {
+                e.target.style.backgroundColor = 'grey';
+                e.target.style.opacity = 0.5;
+                container.instruction.textContent = 'Blank shot!';
+                this.computer.gameBoard.receiveAttack(gridLocation);
+                DOMhelper.controlPointerEvents(
+                    container.computerGrid.children,
+                    'none',
+                );
+                container.computerGrid.style.opacity = 0.5;
+                DOMhelper.controlPointerEvents(
+                    container.playerGrid.children,
+                    'auto',
+                );
+                container.playerGrid.style.opacity = 1;
             }
         }
     }
